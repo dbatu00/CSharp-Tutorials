460aec2e44d8c415d5d30c3a74880c8409b8552b
dbatu00 <denizbatu@sabanciuniv.edu>
2023-04-07 18:40:40 +0300

    .

--- Program Building Blocks/Program.cs
+++ Program Building Blocks/Program.cs
@@ -30,7 +30,7 @@ namespace ProgramBuildingBlocks
         }
 
         public override string ToString() => "This is a color";
-        Program program = new Program();
+        //Program program = new Program(); ???
         
 
 
@@ -40,19 +40,41 @@ namespace ProgramBuildingBlocks
 
     }
 
+    class Entity
+    {
+        //A method declared without a static modifier is an instance method. An instance method
+        //operates on a specific instance and can access both static and instance members. The
+        //instance on which an instance method was invoked can be explicitly accessed as this.
+        //It's an error to refer to this in a static method.
+
+        static int s_nextSerialNo;
+        int _serialNo;
+
+        public Entity()
+        {
+            _serialNo = s_nextSerialNo++;
+        }
+
+        public int GetSerialNo()
+        {
+            return _serialNo;
+        }
+
+        public static int GetNextSerialNo()
+        {
+            return s_nextSerialNo;
+        }
+
+        public static void SetNextSerialNo(int value)
+        {
+            s_nextSerialNo = value;
+        }
+    }
+
 
     class Program
     {
-        public static void PrintName(string firstName, string lastName = "Doe") 
-                          => Console.WriteLine($"Name: {firstName} {lastName}");
-                          //lastName is a default value parameter
-
-        static void Swap(ref int x, ref int y)
-        {
-            int temp = x;
-            x = y;
-            y = temp;
-        }
+        
         static void Main(string[] args)
         {
             Color color = new(5, 5, 5);
@@ -62,5 +84,42 @@ namespace ProgramBuildingBlocks
             Swap(ref i, ref j);
             Console.WriteLine($"{i} {j}");    // "2 1"
         }
+
+        public static void PrintName(string firstName, string lastName = "Doe")
+                          => Console.WriteLine($"Name: {firstName} {lastName}");
+        //lastName is a default value parameter
+
+        static void Swap(ref int x, ref int y) //reference parameter
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        static void Divide(int x, int y, out int quotient, out int remainder)
+        {
+            quotient = x / y;
+            remainder = x % y;
+        }
+
+        public static void OutUsage()
+        {
+            Divide(10, 3, out int quo, out int rem); //out parameter is like a reference parameter
+                                                     //but it doesnt need an inital value
+            Console.WriteLine($"{quo} {rem}");  // "3 1"
+        }
+
+
+        //A parameter array permits a variable number of arguments to
+        //be passed to a method.A parameter array is declared with the
+        //params modifier.Only the last parameter of a method can be a
+        //parameter array, and the type of a parameter array must be a
+        //single-dimensional array type.
+
+        //parameter can be passed as an array or as the elements of the array
+        //Console.WriteLine(s, args);
+        //Console.WriteLine("something", x, y, z);
+
+        public static void Write(string fmt, params object[] args) { } //this is actually a System.Console method
     }
 }
